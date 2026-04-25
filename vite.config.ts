4c9e7684e172b473fde8ade59e75d87fd7908c68
Sukitha Bandara <sukithabandara13@gmail.com>
2026-04-26 00:17:38 +0530

    shell redirect route fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,6 @@
-import { defineConfig } from "@lovable.dev/vite-tanstack-config";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  cloudflare: {},
-  tanstackStart: {
-    spa: {
-      enabled: true,
-    },
-  },
+  plugins: [react()],
 });
